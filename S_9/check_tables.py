63a2c7834fc0c7a8d150642bb1b9bb903ef3c733
abyss <a00411@naver.com>
2025-11-20 20:26:36 +0900

    .

--- S_9/check_tables.py
+++ S_9/check_tables.py
@@ -3,5 +3,5 @@ from database import engine
 with engine.connect() as conn:
     rows = conn.exec_driver_sql(
         "SELECT name FROM sqlite_master WHERE type='table';"
-    ).fetchall()
+    ).fetchall() #SQL 실행 결과를 “모든 행”으로 가져오는 함수.
     print(rows)
